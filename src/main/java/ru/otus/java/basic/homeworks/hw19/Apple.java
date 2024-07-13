3429b2c51889734742cc68da7d25885aeb9db4cc
xa86 <xa86@mail.ru>
2024-07-13 08:56:19 +0300

    Homework nineteen v1

---
+++ src/main/java/ru/otus/java/basic/homeworks/hw19/Apple.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.hw19;
+
+public class Apple extends Fruit {
+    @Override
+    public double getWeight() {
+        return 1.0;
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/hw19/Box.java
@@ -0,0 +1,37 @@
+package ru.otus.java.basic.homeworks.hw19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+    private List<T> fruits;
+
+    public Box() {
+        this.fruits = new ArrayList<>();
+    }
+
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public double getWeight() {
+        double totalWeight = 0.0;
+        for (T fruit : fruits) {
+            totalWeight += fruit.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public boolean compare(Box<?> otherBox) {
+        return Double.compare(this.getWeight(), otherBox.getWeight()) == 0;
+    }
+
+    public void transferFruitsTo(Box<? super T> otherBox) {
+        if (this == otherBox) {
+            return;
+        }
+        otherBox.fruits.addAll(this.fruits);
+        this.fruits.clear();
+    }
+}
+
