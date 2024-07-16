623e2985895fc13ec2c994f6720ff3d4f8f56700
xa86 <xa86@mail.ru>
2024-07-16 09:14:13 +0300

    Homework twenty-one v1

---
+++ src/main/java/ru/otus/java/basic/homeworks/hw21/SynchronizedPrinting.java
@@ -0,0 +1,17 @@
+package ru.otus.java.basic.homeworks.hw21;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SynchronizedPrinting {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Object monitor = new Object();
+
+        executor.submit(new PrintTask('A', monitor, "A", "B"));
+        executor.submit(new PrintTask('B', monitor, "B", "C"));
+        executor.submit(new PrintTask('C', monitor, "C", "A"));
+
+        executor.shutdown();
+    }
+}
